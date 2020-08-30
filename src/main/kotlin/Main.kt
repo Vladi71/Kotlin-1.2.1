9f3ac72bcbf8a5615607ebf9e76dd759bd824cdc
Vladi71 <ronrs6@mail.ru>
2020-08-30 17:13:02 +0300

    Result

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package ru.netology
 
 fun main() {
-
+    val money = 50_000_00
+    val amount = 10_000_00
+    val minCommission = 3500
+    val percentCommission = 0.0075
+    val commission: Int = (amount * percentCommission).toInt()
+    val result = if (commission < minCommission) minCommission else commission
+    val moneyLeft = money - (amount + result)
+    println("Перевод выполнен! Остаток денежных средств: $moneyLeft коп., комиссия составила $result коп.")
 }
